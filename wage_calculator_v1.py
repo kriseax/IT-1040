bdfaf21b31c9a5e9183efd638dc061dde12af8e1
Kristofferson Culmer <krcd58@mail.missouri.edu>
2020-02-12 10:40:47 -0600

    Update wage_calculator_v1.py

--- wage_calculator_v1.py
+++ wage_calculator_v1.py
@@ -1,14 +1,6 @@
 
 
 def main():
-    #Get employee name
-    emp_name = input("Enter employee name (first last): ")
-
-    #Get employee gender
-    emp_gender = input("Enter employee gender (m/f): ")
-                    
-    #Get employee age
-    emp_age = int(input("Enter employee age: "))
 
     #Get employee hours worked
     hours_worked = float(input("Enter hours worked: "))
@@ -20,9 +12,6 @@ def main():
     total_pay = hours_worked * pay_rate
 
     #output name, gender, calculated rate
-    print("Name:", emp_name)
-    print("Gender:", emp_gender)
-    print("Age:", emp_age)
     print("Total Pay:", total_pay,"\n")
 
 main()
