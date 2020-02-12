0e69a06dd9444972393bc3e5278a8ee1a4976346
Kristofferson Culmer <kristofferson@mwc-199182.dhcp.missouri.edu>
2020-02-12 14:50:31 -0600

    Updated code from class demo.

--- wage_calculator_v1.py
+++ wage_calculator_v1.py
@@ -1,17 +1,41 @@
 
-
 def main():
+    while (True):
+        #Get employee hours worked
+        try:
+            hours_worked = float(input("Enter hours worked: "))
 
-    #Get employee hours worked
-    hours_worked = float(input("Enter hours worked: "))
+            #Do not allow 0 or negative values
+            if (hours_worked <= 0):
+                print("Enter a number greater than 0")
+                continue
+            
+        except ValueError as err:
+            print(err)
+            continue
 
-    #Get employee pay rate
-    pay_rate = float(input("Enter pay rate: "))
+        #Get employee pay rate
+        try:
+            pay_rate = float(input("Enter pay rate: "))
 
-    #Calclate total pay
-    total_pay = hours_worked * pay_rate
+            #Do not allow 0 or negative values
+            if (pay_rate <= 0):
+                print("Enter a number greater than 0")
+                continue
+            
+        except ValueError as err:
+            print("Please enter a number")
+            continue
 
-    #output name, gender, calculated rate
-    print("Total Pay:", total_pay,"\n")
+        #Calclate total pay
+        total_pay = hours_worked * pay_rate
+
+        #output name, gender, calculated rate
+        print("Total Pay:", total_pay,"\n")
+
+        do_again = input("Press 'y' to enter more data. Press any other key to exit ")
+
+        if do_again != 'y':
+            break
 
 main()
--- wage_calculator_v2.py
+++ wage_calculator_v2.py
@@ -1,16 +1,39 @@
 #Wage calculator program
 
 def get_float_input(prompt):
+
+    while (True):
+        try:
+            float_value = float(input(prompt))
+
+            #Do not allow 0 or negative values
+            if float_value <= 0:
+                print("Enter a value greater than 0")
+                continue
+        except ValueError as err:
+            print("Please enter a number")
+        else:
+            break
             
-    return 
+    return float_value
 
 def get_string_input(prompt):
-   
-    return 
+    while True:
+        string_value = input(prompt)
+
+        #Do not allow an empty string
+        if string_value == "":
+            print("Please enter a value")
+            continue
+        else:
+            break
+        
+    return string_value
 
 def get_integer_input(prompt):
-    
-    return 
+    integer_value = input(prompt)
+
+    return integer_value
     
 def main():
 
@@ -24,7 +47,7 @@ def main():
         emp_gender = get_string_input("Enter employee gender (m/f): ")
             
         #Get employee age
-        emp_age = get_integer_input("Enter employee age: ")
+        #emp_age = get_integer_input("Enter employee age: ")
 
         #Get employee hours worked
         hours_worked = get_float_input("Enter hours worked: ")
@@ -33,12 +56,13 @@ def main():
         pay_rate = get_float_input("Enter pay rate: ")
 
         #Calclate total pay
-        #total_pay = hours_worked * pay_rate
+        total_pay = hours_worked * pay_rate
 
         #output name, gender, calculated rate
+        print()
         print("Name:", emp_name)
         print("Gender:", emp_gender)
-        print("Age:", emp_age)
+        #print("Age:", emp_age)
         print("Total Pay:", total_pay,"\n")
 
         #Ask user if they would like to enter more data
